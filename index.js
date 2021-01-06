15bd6e19452b5d2172c1d8dacfba5f17d8af24ca
John McDonald <jmcdonald1991@gmail.com>
2021-01-06 04:09:54 -0600

    fs read write

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
 
-const greeting = "Hello World";
-console.log(greeting);
+const output = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(output);
+
+const textOutput = `This is what we know about the avocado: ${output}.\nCreated on ${Date.now()}`;
+
+fs.writeFileSync("./txt/output.txt", textOutput);
